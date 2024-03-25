1d0ecc71c7bca3b6deb5eb86ea130dda86e75fc8
peilin <peilin.liao@kellogg.northwestern.edu>
2024-03-25 14:58:53 -0500

    update run script

---
+++ scripts/rag/run_batch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -A your_quest_allocation_account
+#SBATCH -p gengpu
+#SBATCH --gres=gpu:a100:1
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -t 0:30:00
+#SBATCH --mem=40G
+
+
+module purge
+module load mamba/23.1.0
+source /hpc/software/mamba/23.1.0/etc/profile.d/conda.sh
+source activate /kellogg/software/envs/gpu-llama2
+
+python proc_llamaindex_short.py
+
