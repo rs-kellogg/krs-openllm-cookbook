90e704735ddbc82baf8aae2cd17a276138b553b4
ambreenchaudhri <49079761+ambreenchaudhri@users.noreply.github.com>
2024-04-03 07:49:58 -0500

    Update llama2_test.py

--- scripts/llama_cpp_python/llama2_test.py
+++ scripts/llama_cpp_python/llama2_test.py
@@ -1,23 +1,47 @@
-#################################
-# Llama2 test code with llama_cpp
-#################################
-
+###################################
+# Llama2 model with llama_cpp_python
+###################################
 # libraries
 from llama_cpp import Llama
 
-# inputs
-my_model_path = "/model/llama-2-7b-chat.Q5_K_M.gguf"
-max_l = 5000
+#########
+# Inputs
+model_path = "/model/llama-2-7b-chat.Q5_K_M.gguf"
+context_size = 512
+max_tokens_select = 1000
+temperature_select: float=0
+top_p_select: float=0.9
+top_k_select: int=0
 
-# load the model
+########
+# Prompt
+#prompt = "Can you provide a summary of Guy Debord's Societe du Spectacle?"
+prompt = "What kind of pickups are on an ESP LTD Alexi Ripped?"
+
+# prompt written in gemma prompt syntax
+prompt_sytnax = "<start_of_turn>user" + prompt + "<end_of_turn>" + "<start_of_turn>model"
+
+############
+# load model
 llm = Llama(
-  model_path=my_model_path,  # The path to the model file
-  n_threads=4,  # The number of CPU threads to use
-  n_gpu_layers=0  # Set to 0 if you want to use CPU only and -1 if you want to use all available GPUs
+  model_path=model_path,  # The path to the model file
+  n_ctx=context_size,  # The max sequence length to use - adjust based on your model's requirements
+  n_threads=1,  # The number of CPU threads to use
+  n_gpu_layers=-1  # Set to 0 if you want to use CPU only and -1 if you want to use all available GPUs
 )
 
-message = "What's the most popular ice cream flavor?"
-response = llm(message, max_tokens=max_l, temperature=0)
-generated_text = response["choices"][0]["text"]
-print(f"System: {generated_text}")
+##############
+# send prompt
+response = llm(
+    prompt,
+    max_tokens=max_tokens_select, 
+    temperature=temperature_select,
+    top_p=top_p_select,
+    top_k=top_k_select,
+    echo = False
+    )
 
+##############
+# get response
+response_text = response['choices'][0]['text']
+print(response_text)
