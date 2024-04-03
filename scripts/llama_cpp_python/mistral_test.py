8b43fceb5aa5dc601283a4f9bab224b0cf235924
ambreenchaudhri <49079761+ambreenchaudhri@users.noreply.github.com>
2024-04-03 08:09:34 -0500

    Update mistral_test.py

--- scripts/llama_cpp_python/mistral_test.py
+++ scripts/llama_cpp_python/mistral_test.py
@@ -1,36 +1,48 @@
-#################################
-# Mistral test code with llama_cpp
-#################################
-
+###################################
+# Mistral model with llama_cpp_python
+###################################
 # libraries
 from llama_cpp import Llama
 
-# inputs
-my_model_path = "/model/mistral-7b-v0.1.Q5_K_S.gguf"
-CONTEXT_SIZE = 512
+#########
+# Inputs
+model_path = "/model/mistral-7b-v0.1.Q5_K_S.gguf"
+context_size = 512
+max_tokens_select = 20
+temperature_select: float=0
+top_p_select: float=0.9
+top_k_select: int=0
 
-# load the model
-llm = Llama(
-  model_path=my_model_path,  # The path to the model file
-  n_ctx=CONTEXT_SIZE,  # The max sequence length to use - adjust based on your model's requirements
-  n_threads=10,  # The number of CPU threads to use
-  n_gpu_layers=0  # Set to 0 if you want to use CPU only and -1 if you want to use all available GPUs
-)
-
-# send prompts
-#prompt = """
-#[INST] What's the most popular ice cream flavor? Answer with one or two words. For example: 'mint chocolate chip'
-#[/INST]
-#"""
-
-prompt = """<s>[INST] You are a helpful assistant. What's the frequency, Kenneth? Please answer by providing a
-randomly generated frequency. DO NOT quote R.E.M.  Do not tell me what you think about Generation Alpha only to be
-proven wrong. Do not respond to this question more than once.  Here is an example answer:
-[/INST]'20 Hz'</s>
-[INST] Plesae provide only one frequency.[/INST]
+########
+# Prompt
+prompt = "Please randomly select an ice cream flavor.  Here is an example: rocky road. Now you give me one."
+prompt_syntax = """<s>[INST] Please randomly select an ice cream flavor.  Here is an example answer:
+[/INST]'rocky road'</s>
 """
 
-temp: float = 0
-response = llm(prompt, max_tokens=1000, temperature=temp)
+############
+# load model
+llm = Llama(
+  model_path=model_path,  # The path to the model file
+  n_ctx=context_size,  # The max sequence length to use - adjust based on your model's requirements
+  n_threads=1,  # The number of CPU threads to use
+  n_gpu_layers=-1  # Set to 0 if you want to use CPU only and -1 if you want to use all available GPUs
+)
+
+
+
+##############
+# send prompt
+response = llm(
+    prompt,
+    max_tokens=max_tokens_select, 
+    temperature=temperature_select,
+    top_p=top_p_select,
+    top_k=top_k_select,
+    echo = False
+    )
+
+##############
+# get response
 response_text = response['choices'][0]['text']
 print(response_text)
